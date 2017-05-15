ee09994c82d81e7803c6f2dd75c0155f9a018f1f
蒲刚强 <pugangqiang@jd.com>
2017-05-15 17:24:33 +0800

    FIX:BUILD ERROR

--- src/main/java/jd/dd/waiter/ui/dialog/DDDialogUtils.java
+++ src/main/java/jd/dd/waiter/ui/dialog/DDDialogUtils.java
@@ -2,14 +2,12 @@ package jd.dd.waiter.ui.dialog;
 
 import android.content.Context;
 
-import jd.dd.waiter.tcp.UserInfo;
-
 /**
  * Created by pugq on 2017/5/15/015.
  */
 
 public class DDDialogUtils {
-    public static void showForceLogoutDialog(Context context, UserInfo userInfo, String msg) {
+    public static void showForceLogoutDialog(Context context, Object userInfo, String msg) {
 
     }
 }
