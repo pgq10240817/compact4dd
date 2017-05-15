5baae5d59091cba9c1424c96dfa56761c0fc8931
蒲刚强 <pugangqiang@jd.com>
2017-05-15 17:18:04 +0800

    ADD:dialog 依赖

---
+++ src/main/java/jd/dd/waiter/ui/dialog/DDDialogUtils.java
@@ -0,0 +1,15 @@
+package jd.dd.waiter.ui.dialog;
+
+import android.content.Context;
+
+import jd.dd.waiter.tcp.UserInfo;
+
+/**
+ * Created by pugq on 2017/5/15/015.
+ */
+
+public class DDDialogUtils {
+    public static void showForceLogoutDialog(Context context, UserInfo userInfo, String msg) {
+
+    }
+}
